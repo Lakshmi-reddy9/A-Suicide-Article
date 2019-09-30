11d218f9f4d2bad8b92c8c7ae6114c4d48ee7a21
Rayaan Ahmed <raybox94@gmail.com>
2019-09-30 16:51:50 -0500

    Add code to mapper and reducer

---
+++ year/mapper.py
@@ -0,0 +1,17 @@
+input_mapper = open("../dataset/master.txt","r") # Opening data file in read-only mode 
+output_mapper = open("output_mapper.txt","w") # Opening output_mapper file in write mode (It will create file if file doesnot exist)
+# For loop for reading each line from the text file
+for line in input_mapper:
+    # Seperating data by (,) and sending it to data array
+    data = line.strip().split(',')
+    # Checking the size of array
+    if (len(data) == 12):
+        # Assigning data to different variables
+        country, year, sex, age, suicides_number, population, suicides_per_pop, country_year, hdi_per_year, gdp_per_year, gdp_per_captal, generation = data
+        # Writing year and suicides_number to output file
+        output_mapper.write(year+","+suicides_number+"\n")
+        # printing year and suicides_number to console
+        print(year+","+suicides_number+"\n")
+# Closing all files
+input_mapper.close()
+output_mapper.close()
